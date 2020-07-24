b14145c312bbcc140135396125021d7b86b59265
oratake <smith.shimomura@gmail.com>
2020-07-24 16:46:43 +0900

    upd: users table add login_name, user_name restriction

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('user_name');
+            $table->string('login_name')->unique()->comment('ログインに使用するシングルバイト文字列');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
