e49013d81a0c56bbff9e3c7073c77e1d3067519a
oratake <smith.shimomura@gmail.com>
2020-09-19 15:35:01 +0900

    add: controller article.show

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -36,4 +36,9 @@ class ArticleController extends Controller
     {
         return view('articles.edit', ['article' => $article]);
     }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }
 }
